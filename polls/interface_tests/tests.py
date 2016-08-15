3afc0a82ad1d5e54206db69789ba97c8fa3caf76
defnngj <defnngj@gmail.com>
2016-08-15 09:56:14 +0800

    modify test cases

--- polls/interface_tests/tests.py
+++ polls/interface_tests/tests.py
@@ -1,7 +1,7 @@
 __author__ = 'bugmaster'
 import unittest
 import requests
-import json
+
 
 '''
 Need to install requests Library
@@ -16,7 +16,7 @@ class QuestionsTest(unittest.TestCase):
     def test_get_questions_success(self):
         '''get all questions success'''
         r = requests.get(self.base_url)
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 200)
         self.assertEqual(result['message'], 'success')
 
@@ -29,21 +29,21 @@ class QuestionOptionTest(unittest.TestCase):
     def test_get_question_option_success(self):
         '''get question option success'''
         r = requests.get(self.base_url,params={'qid':'1'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 200)
         self.assertEqual(result['message'], 'success')
 
     def test_get_question_option_qid_null(self):
         '''get question option qid null'''
         r = requests.get(self.base_url,params={'qid':''})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10021)
         self.assertEqual(result['message'], 'Parameter error')
 
     def test_get_question_option_qid_error(self):
         '''get question option qid error'''
         r = requests.get(self.base_url,params={'qid':'901'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10022)
         self.assertEqual(result['message'], 'Query result is empty')
 
@@ -56,21 +56,21 @@ class QuestionResultTest(unittest.TestCase):
     def test_get_question_result_success(self):
         '''get question result success'''
         r = requests.get(self.base_url,params={'qid':'1'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 200)
         self.assertEqual(result['message'], 'success')
 
     def test_get_question_result_qid_null(self):
         '''get question result qid null'''
         r = requests.get(self.base_url,params={'qid':''})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10021)
         self.assertEqual(result['message'], 'Parameter error')
 
     def test_get_question_result_qid_error(self):
         '''get question result qid error'''
         r = requests.get(self.base_url,params={'qid':'901'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10022)
         self.assertEqual(result['message'], 'Query result is empty')
 
@@ -83,35 +83,35 @@ class QuestionVoteTest(unittest.TestCase):
     def test_post_question_vote_qid_null(self):
         '''get question vote qid null'''
         r = requests.post(self.base_url, data={'qid': '','cid':'2'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10021)
         self.assertEqual(result['message'], 'Parameter error')
 
     def test_post_question_vote_cid_null(self):
         '''get question vote cid null'''
         r = requests.post(self.base_url, data={'qid': '1','cid':''})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10021)
         self.assertEqual(result['message'], 'Parameter error')
 
     def test_post_question_vote_qid_error(self):
         '''get question vote qid error'''
         r = requests.post(self.base_url, data={'qid': '901','cid':'1'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10022)
         self.assertEqual(result['message'], 'Query result is empty')
 
     def test_post_question_vote_cid_error(self):
         '''get question vote cid error'''
         r = requests.post(self.base_url, data={'qid': '1','cid':'901'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 10023)
         self.assertEqual(result['message'], 'The problem is not the choice id')
 
     def test_post_question_vote_success(self):
         '''get question vote success'''
         r = requests.post(self.base_url, data={'qid': '1','cid':'1'})
-        result = json.loads(r.text)
+        result = r.json()
         self.assertEqual(result['ststus'], 200)
         self.assertEqual(result['message'], 'success')
 
